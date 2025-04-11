0a1293b758d5bf2fd9f776355d99d11a4af7ff4e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-11 11:07:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,8 @@ fig = px.scatter_mapbox(
     hover_data=[col for col in dados.columns if col not in ["latitude", "longitude"]],
     zoom=6,
     height=1000,
-    title=f"Localização de {label} no RS"
+    title=f"Localização de {label} no RS",
+    center = {'lat':-29.53668155059797, 'lon':-53.03101633519228}
 )
 
 fig.update_layout(mapbox_style="open-street-map")
