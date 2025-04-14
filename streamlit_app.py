872b320b094bdc73ea94ea02da4a38eafb422e6e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-14 15:53:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,15 +123,25 @@ fig.update_geos(
         lonaxis=dict(range=[-58, -48]),
         center=dict(lat=-30.537, lon=-52.965),
         visible = False,
-        projection_scale= 5,
+        projection_scale= 10,
         
 )
 fig.update_layout(
         margin={"r": 0, "t": 30, "l": 0, "b": 0},
         height=800,
-        title=f"Localização de {label} e Alertas INMET no RS"
+        title=f"Localização de {label} e Alertas INMET no RS",
+    geo = dict(
+        showland=True,
+        landcolor="rgb(229, 229, 229)",
+        lataxis=dict(range=[-34, -26]),  # ajustado para RS
+        lonaxis=dict(range=[-58, -48]),
+        center=dict(lat=-30.537, lon=-52.965),
+        visible = False,
+        projection_scale= 10,
+
+    )
     
 )
 
 # Exibe o mapa
-st.plotly_chart(fig)#, use_container_width=True, config={"scrollZoom": True})
+st.plotly_chart(fig, use_container_width=True, config={"scrollZoom": True})
