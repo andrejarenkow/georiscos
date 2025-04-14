232d73c2aeea1a20b064572d0dd3c68c33373078
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-14 15:43:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,7 +123,7 @@ fig.update_geos(
         lonaxis=dict(range=[-58, -48]),
         center=dict(lat=-30.537, lon=-52.965),
         visible = False,
-        margin={"r": 0, "t": 30, "l": 0, "b": 0},
+        #margin={"r": 0, "t": 30, "l": 0, "b": 0},
         height=800,
         title=f"Localização de {label} e Alertas INMET no RS"
 )
