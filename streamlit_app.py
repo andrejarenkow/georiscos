ecc8681554efdac4e7dd9924610e956a4c52bc2c
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-14 15:29:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ geojson_data = obter_alertas_rs()
 fig = go.Figure()
 
 # Adiciona os pontos de hospitais/UBS
-fig.add_trace(go.Scattermapbox(
+fig.add_trace(go.Scattermap(
     lat=dados["latitude"],
     lon=dados["longitude"],
     mode='markers',
@@ -99,7 +99,7 @@ for feature in geojson_data["features"]:
     descricao = props.get("descricao", "Alerta")
     estados = props.get("estados", "")
 
-    fig.add_trace(go.Scattermapbox(
+    fig.add_trace(go.Scattermap(
         lat=lat,
         lon=lon,
         mode='lines',
