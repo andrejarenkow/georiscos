29223ffd3623b10f0273c525106c21529953fb52
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-28 17:53:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -352,9 +352,12 @@ with coluna_metricas:
     st.metric('Barragens em área de alerta', value = len(barragens_dentro))
 
 # Implementação de chat
-with st.sidebar:
-    messages = st.container(height=300)
-    if prompt := st.chat_input("Pergunte algo"):
-        messages.chat_message("user").write(prompt)
-        messages.chat_message("assistant").write(f"Echo: {prompt}")
+@st.fragment
+def my_fragment():
+    with st.sidebar:
+        messages = st.container(height=300)
+        if prompt := st.chat_input("Pergunte algo"):
+            messages.chat_message("user").write(prompt)
+            messages.chat_message("assistant").write(f"Echo: {prompt}")
 
+my_fragment()
